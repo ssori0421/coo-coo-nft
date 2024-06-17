129841c15ac872a488aecc53cb48503dc506264e
ssori <115159126+ssori0421@users.noreply.github.com>
2024-06-17 22:14:41 +0900

    feat: useApprovalStatus(), useNftMetadata() 커스텀 훅 적용 및 MyNftCard 컴포넌트를 적용하여 MyCoocooPage 생성

--- vite/src/pages/myCoocooPage.tsx
+++ vite/src/pages/myCoocooPage.tsx
@@ -1,14 +1,131 @@
-import { FC } from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex, Grid, Text, Switch } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import { IOutletContext } from '../components/Layout';
+import MyNftCard from '../components/MyNftCard';
+import useApprovalStatus from '../hooks/useApprovalStatue';
+import useNftMetadata from '../hooks/useNftMetadata';
 
-const MyCoocooPage: FC = () => {
+const PAGE = 3;
+
+const MyCoocooPage = () => {
+  const [balanceOf, setBalanceOf] = useState<number>(0);
+
+  const { mintContract, signer, saleContract } =
+    useOutletContext<IOutletContext>();
+
+  const { isApprovedForAll, isApproveLoading, onClickSetApprovalForAll } =
+    useApprovalStatus({ mintContract, signer });
+
+  const { nftMetadataArray, tokenIds, isEnd, isLoading, getNftMetadata } =
+    useNftMetadata({ mintContract, signer, balanceOf, PAGE });
+
+  const getBalanceOf = async () => {
+    try {
+      const address = await signer?.getAddress();
+      const response = await mintContract?.balanceOf(address);
+      setBalanceOf(Number(response));
+    } catch (error) {
+      console.error('Error fetching balance:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (mintContract && signer) {
+      getBalanceOf();
+    }
+  }, [mintContract, signer]);
+
   return (
     <Flex
       flexDirection='column'
       w='100%'
       background='linear-gradient(to bottom, #f1856a, #ce48c1)'
+      py={8}
+      px={20}
+      alignItems='center'
+      minW={530}
     >
-      MyCoocooPage
+      {balanceOf ? (
+        <Flex
+          alignItems='center'
+          gap={4}
+          mb={6}
+          p={4}
+          borderRadius='24px'
+          bg='white'
+          boxShadow='md'
+          justifyContent={['center', 'center', 'flex-end']}
+          ml={[0, 0, 'auto']}
+        >
+          <Text fontSize='lg' fontWeight='bold' fontFamily='DNFBitBitTTF'>
+            판매 여부
+          </Text>
+          <Switch
+            colorScheme='blue'
+            isChecked={isApprovedForAll}
+            onChange={onClickSetApprovalForAll}
+            isDisabled={isApproveLoading}
+          />
+        </Flex>
+      ) : null}
+
+      <Flex mb={8} gap={2}>
+        <Text
+          color='white'
+          fontSize={[28, 32, 36]}
+          fontWeight='bold'
+          fontFamily='DNFBitBitTTF'
+        >
+          MY COO-COO NFT
+        </Text>
+        <Text
+          color='white'
+          fontSize={[28, 32, 36]}
+          fontWeight='bold'
+          fontFamily='DNFBitBitTTF'
+        >
+          : {balanceOf} 개
+        </Text>
+      </Flex>
+
+      {balanceOf ? (
+        <>
+          <Grid
+            templateColumns={[
+              'repeat(1, 1fr)',
+              'repeat(2, 1fr)',
+              'repeat(3, 1fr)',
+            ]}
+            gap={6}
+          >
+            {nftMetadataArray.map((metadata, index) => (
+              <MyNftCard
+                key={index}
+                nftMetadata={metadata}
+                tokenId={tokenIds[index]}
+                saleContract={saleContract}
+                isApprovedForAll={isApprovedForAll}
+              />
+            ))}
+          </Grid>
+          {!isEnd && (
+            <Button
+              mt={8}
+              onClick={getNftMetadata}
+              isDisabled={isLoading}
+              isLoading={isLoading}
+              loadingText='로딩중'
+            >
+              더 보기
+            </Button>
+          )}
+        </>
+      ) : (
+        <Text color='white' fontSize={24} fontWeight='semibold'>
+          지갑을 연결해주세요 :)
+        </Text>
+      )}
     </Flex>
   );
 };
