017bf426d7e7c68722de84dce2584b5d9467f70e
ssori <115159126+ssori0421@users.noreply.github.com>
2024-06-17 14:43:16 +0900

    feat: GardenNftCard 컴포넌트를 적용하여 coocooGardenPage UI 생성

--- vite/src/pages/coocooGardenPage.tsx
+++ vite/src/pages/coocooGardenPage.tsx
@@ -1,14 +1,63 @@
-import { FC } from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Grid, Text } from '@chakra-ui/react';
+import { useOutletContext } from 'react-router-dom';
+import { IOutletContext } from '../components/Layout';
+import { useEffect, useState } from 'react';
+import GardenNftCard from '../components/GardenNftCard';
 
-const CooCooGardenPage: FC = () => {
+const CooCooGardenPage = () => {
+  const [tokenIds, setTokenIds] = useState<number[]>([]);
+
+  const { signer, saleContract, mintContract } =
+    useOutletContext<IOutletContext>();
+
+  const getOnSaleTokens = async () => {
+    try {
+      const response = await saleContract?.getOnSaleTokens();
+
+      const tempTokenIds = response.map((item: any) => {
+        return Number(item);
+      });
+
+      setTokenIds(tempTokenIds);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!saleContract) return;
+
+    getOnSaleTokens();
+  }, [saleContract]);
+
+  useEffect(() => {
+    console.log(tokenIds);
+  }, [tokenIds]);
+
   return (
     <Flex
       flexDirection='column'
       w='100%'
       background='linear-gradient(to bottom, #f1856a, #ce48c1)'
+      py={8}
+      px={20}
     >
-      CooCooGarden
+      <Grid
+        templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']}
+        gap={8}
+      >
+        {tokenIds.map((item, index) => (
+          <GardenNftCard
+            key={index}
+            tokenId={item}
+            mintContract={mintContract}
+            saleContract={saleContract}
+            signer={signer}
+            tokenIds={tokenIds}
+            setTokenIds={setTokenIds}
+          />
+        ))}
+      </Grid>
     </Flex>
   );
 };
