0b67fd21eef71874311c403729e2a19da85b5cb3
ssori <115159126+ssori0421@users.noreply.github.com>
2024-06-18 16:29:11 +0900

    style: nft카드에 마우스 호버시 줬던 배경으로 인해 구매 버튼 클릭 이벤트가 발생하지 않아 호버시 스케일이 커지도록 스타일 수정

--- vite/src/components/GardenNftCard.tsx
+++ vite/src/components/GardenNftCard.tsx
@@ -88,19 +88,10 @@ const GardenNftCard = ({
         overflow='hidden'
         onClick={() => onClickCardDetail(tokenId)}
         _hover={{
-          _before: {
-            content: '""',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            bg: 'rgba(255, 255, 255, 0.7)',
-            borderRadius: '12px',
-            zIndex: 5,
-          },
-          cursor: 'pointer',
+          transform: 'scale(1.05)',
+          transition: 'transform 0.3s ease-in-out',
         }}
+        cursor='pointer'
       >
         <Flex flexDirection='column' zIndex={2}>
           <Flex alignItems='center' justifyContent='space-between'>
