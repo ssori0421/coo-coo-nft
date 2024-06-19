29be48d81305ef95560cd2b5f7ed1f3b49d7db3e
ssori <115159126+ssori0421@users.noreply.github.com>
2024-06-19 14:54:55 +0900

    feat: homePage 이미지 슬라이드 관련 이미지 데이터 작성

---
+++ vite/src/lib/homeSlideImages.ts
@@ -0,0 +1,51 @@
+import cooing1 from '../../public/homeCoocooImages/cooing1.png';
+import cooing2 from '../../public/homeCoocooImages/cooing2.png';
+import cooing3 from '../../public/homeCoocooImages/cooing3.png';
+import cooing4 from '../../public/homeCoocooImages/cooing4.png';
+import cooing5 from '../../public/homeCoocooImages/cooing5.png';
+import cooing6 from '../../public/homeCoocooImages/cooing6.png';
+import cooing7 from '../../public/homeCoocooImages/cooing7.png';
+import cooing8 from '../../public/homeCoocooImages/cooing8.png';
+import cooing9 from '../../public/homeCoocooImages/cooing9.png';
+import cooing10 from '../../public/homeCoocooImages/cooing10.png';
+import cooing11 from '../../public/homeCoocooImages/cooing11.png';
+import cooing12 from '../../public/homeCoocooImages/cooing12.png';
+import cooing13 from '../../public/homeCoocooImages/cooing13.png';
+import cooing14 from '../../public/homeCoocooImages/cooing14.png';
+import cooing15 from '../../public/homeCoocooImages/cooing15.png';
+import cooing16 from '../../public/homeCoocooImages/cooing16.png';
+import cooing17 from '../../public/homeCoocooImages/cooing17.png';
+import cooing18 from '../../public/homeCoocooImages/cooing18.png';
+import cooing19 from '../../public/homeCoocooImages/cooing19.png';
+import cooing20 from '../../public/homeCoocooImages/cooing20.png';
+
+interface IHomeSlideImagesData {
+  id: number;
+  name: string;
+  src: string;
+}
+
+const homeSlideImagesData: IHomeSlideImagesData[] = [
+  { id: 1, name: 'cooing1', src: cooing1 },
+  { id: 2, name: 'cooing2', src: cooing2 },
+  { id: 3, name: 'cooing3', src: cooing3 },
+  { id: 4, name: 'cooing4', src: cooing4 },
+  { id: 5, name: 'cooing5', src: cooing5 },
+  { id: 6, name: 'cooing6', src: cooing6 },
+  { id: 7, name: 'cooing7', src: cooing7 },
+  { id: 8, name: 'cooing8', src: cooing8 },
+  { id: 9, name: 'cooing9', src: cooing9 },
+  { id: 10, name: 'cooing10', src: cooing10 },
+  { id: 11, name: 'cooing11', src: cooing11 },
+  { id: 12, name: 'cooing12', src: cooing12 },
+  { id: 13, name: 'cooing13', src: cooing13 },
+  { id: 14, name: 'cooing14', src: cooing14 },
+  { id: 15, name: 'cooing15', src: cooing15 },
+  { id: 16, name: 'cooing16', src: cooing16 },
+  { id: 17, name: 'cooing17', src: cooing17 },
+  { id: 18, name: 'cooing18', src: cooing18 },
+  { id: 19, name: 'cooing19', src: cooing19 },
+  { id: 20, name: 'cooing20', src: cooing20 },
+];
+
+export default homeSlideImagesData;
