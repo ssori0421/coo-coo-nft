935f69ef0d8b318a9c4283f966c45d5602c0f826
ssori <115159126+ssori0421@users.noreply.github.com>
2024-06-20 11:41:39 +0900

    HOTFIX: 컨트랙트 재발행으로 주소 수정

--- vite/src/lib/contractAddress.ts
+++ vite/src/lib/contractAddress.ts
@@ -1,2 +1,2 @@
-export const mintContractAddress = '0x7889dA1b121CC6c7634F319639450847CcCe20c9';
-export const saleContractAddress = '0xf9178c129675eF7d2D3a50aC2Cc7BD0F19023e67';
+export const mintContractAddress = '0x58c8bE20ab9d8ba90d5C372c5705E7a2d82dB442';
+export const saleContractAddress = '0x59aEc4fbc99A4a708e6Be0D7778f88E488a4aE9D';
